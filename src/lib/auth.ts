4c623d16f50cce92223507bc6d6610c73578c08c
isaquerchaves <isaque.ramosch@gmail.com>
2023-10-22 15:20:43 -0300

    fix: formatting auth.ts and solving the bug

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,12 +3,12 @@ import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 
-export const authOptions:AuthOptions {
-    adapter: PrismaAdapter(prismaClient),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      }),
-    ],
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 };
