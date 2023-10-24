a1c17ce581210aa3f4920c4607dc228d9e218224
isaquerchaves <isaque.ramosch@gmail.com>
2023-10-24 00:30:26 -0300

    feat: ensure user goers to home page on home item click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -92,10 +92,17 @@ const Header = () => {
               </Button>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} />
-              Início
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
@@ -117,12 +124,11 @@ const Header = () => {
         </SheetContent>
       </Sheet>
 
-        <Link href="/">
+      <Link href="/">
         <h1 className="text-lg font-semibold">
-        <span className="text-primary">IRC</span> Store
-      </h1>
-        </Link>
-      
+          <span className="text-primary">IRC</span> Store
+        </h1>
+      </Link>
 
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
