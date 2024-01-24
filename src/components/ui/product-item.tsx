692ca07f8afe85d7ece1b113985d40cc882604af
isaquerchaves <isaque.ramosch@gmail.com>
2024-01-24 17:16:09 -0300

    styled: ajustando responsividade da barra de rolagem de produtos da home page

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,59 +2,64 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import { ArrowDownIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
+  className?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <Link href={`/product/${product.slug}`} className="flex min-w-[156px] flex-col gap-4">
-      <div className="flex flex-col gap-4">
-        <div className="relative flex aspect-square w-full items-center justify-center rounded-lg bg-accent">
-          <Image
-            src={product.imageUrls[0]}
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto max-h-[70%] w-auto max-w-[80%] "
-            // Fazer com que o produto não perca o ASPECT RATIO
-            style={{
-              objectFit: "contain",
-            }}
-            alt={product.name}
-          />
+    <Link
+      href={`/product/${product.slug}`}
+      className={cn("flex min-w-[156px] flex-col gap-4", className)}
+    >
+      <div className="relative flex aspect-square w-full items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          alt={product.name}
+        />
 
-          {product.discountPercentage > 0 && (
-            <div className="absolute left-2 top-2 inline-flex items-center rounded-full border bg-primary px-2 py-[2px] text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
-              <ArrowDownIcon size={14} /> {product.discountPercentage}%
-            </div>
-          )}
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-            {product.name}
-          </p>
-
-          <div className="flex items-center gap-2">
-            {product.discountPercentage > 0 ? ( // Se o produto tiver desconto, vai mostrar o preço com desconto
-              <>
-                <p className="font-semibold">
-                  R$ {product.totalPrice.toFixed(2)}
-                </p>
-                <p className="text-xs line-through opacity-75">
-                  R$ {Number(product.basePrice).toFixed(2)}
-                </p>
-              </>
-            ) : (
-              // Se o produto não tiver desconto, vai mostrar o preço sem desconto
-              <p className="text-sm font-semibold">
-                R$ {product.totalPrice.toFixed(2)}
-              </p>
-            )}
+        {product.discountPercentage > 0 && (
+          <div className="absolute left-2 top-2 inline-flex items-center rounded-full border bg-primary px-2 py-[2px] text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+            <ArrowDownIcon size={14} /> {product.discountPercentage}%
           </div>
+        )}
+      </div>
 
+      <div className="flex flex-col gap-1">
+        <p className="truncate text-sm">{product.name}</p>
+
+        <div className="flex items-center gap-2 ">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="truncate font-semibold lg:text-lg">
+                {product.totalPrice.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+
+              <p className="truncate text-xs line-through opacity-75 lg:text-sm">
+                {Number(product.basePrice).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+            </>
+          ) : (
+            <p className="truncate text-sm font-semibold">
+              {Number(product.basePrice).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </p>
+          )}
         </div>
       </div>
     </Link>
--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -11,11 +11,11 @@ const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-        <div key={product.id} className="max-w[170px] w-[170px]">
-          <ProductItem
-            product={computerProductTotalPrice(product)}
-          />
-        </div>
+        <ProductItem
+          key={product.id}
+          product={computerProductTotalPrice(product)}
+          className="w-[156px] lg:w-[200px] lg:min-w-[200px]"
+        />
       ))}
     </div>
   );
